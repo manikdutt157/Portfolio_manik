b0d83068dd85275463d6cf0619831664b41730cc
Manik Dutt <manikdutt157@gmail.com>
2024-09-01 23:51:37 +0530

    Created and Started Project

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="w-full h-screen bg-black text-white">
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='shadow-md w-full fixed top-0 left-0'>
+      <div className="md:flex bg-white py-4">
+        <div className="">
+          <span></span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
